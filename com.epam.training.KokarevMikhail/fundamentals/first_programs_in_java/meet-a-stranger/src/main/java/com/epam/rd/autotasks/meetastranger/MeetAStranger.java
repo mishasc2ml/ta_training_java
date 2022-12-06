eb483ffb052431e5b8291df76bf0dbd9f80b1927
mishasc2ml <mkokarev989898@gmail.com>
2022-12-06 16:19:22 +0300

    meet-a-stranger solved

---
+++ com.epam.training.KokarevMikhail/fundamentals/first_programs_in_java/meet-a-stranger/src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -0,0 +1,10 @@
+package com.epam.rd.autotasks.meetastranger;
+
+import java.util.Scanner;
+
+public class MeetAStranger {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Hello, " + console.nextLine());
+    }
+}
