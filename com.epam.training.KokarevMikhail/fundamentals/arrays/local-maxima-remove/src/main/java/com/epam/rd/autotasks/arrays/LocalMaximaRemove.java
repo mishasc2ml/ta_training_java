2ceb7d5d299e702cf788fd67cce11933632ff3cb
mishasc2ml <mkokarev989898@gmail.com>
2022-12-06 16:53:01 +0300

    local maxima remove solved

---
+++ com.epam.training.KokarevMikhail/fundamentals/arrays/local-maxima-remove/src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -0,0 +1,21 @@
+package com.epam.rd.autotasks.arrays;
+
+import java.util.Arrays;
+public class LocalMaximaRemove {
+    public static void main(String[] args) {
+        int[] array = new int[]{18, 1, 3, 6, 7, -5};
+
+        System.out.println(Arrays.toString(removeLocalMaxima(array)));
+    }
+    public static int[] removeLocalMaxima(int[] array) {
+        int[] arrRes = new int[array.length];
+        int j = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if ((i > 0 && array[i] <= array[i - 1]) || (i != array.length - 1 && array[i] <= array[i + 1])) {
+                arrRes[j++] = array[i];
+            }
+        }
+        return Arrays.copyOf(arrRes, j);
+    }
+}
