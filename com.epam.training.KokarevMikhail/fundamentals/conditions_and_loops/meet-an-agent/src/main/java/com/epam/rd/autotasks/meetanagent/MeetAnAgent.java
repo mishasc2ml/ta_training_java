058391058e3ba8669ed993fc989d1693f2997aac
mishasc2ml <mkokarev989898@gmail.com>
2022-12-06 16:26:05 +0300

    meet-an-agent solved

---
+++ com.epam.training.KokarevMikhail/fundamentals/conditions_and_loops/meet-an-agent/src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -0,0 +1,12 @@
+package com.epam.rd.autotasks.meetanagent;
+
+import java.util.Scanner;
+
+public class MeetAnAgent {
+    final static int PASSWORD = 133976; //You can change pass, but don't change the type
+
+    public static void main(String[] args) {
+        String res = new Scanner(System.in).nextInt() == PASSWORD ? "Hello, Agent" : "Access denied";
+        System.out.println(res);
+    }
+}
